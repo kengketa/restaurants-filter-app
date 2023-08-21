bfc11a7fed71982949a2a4f5de993c519d1d873c
Kengketa <kengketa@gmail.com>
2023-08-21 18:34:56 +0700

    feat: Add custom request to filter before hitting controller

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Actions\FetchRestaurantAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\FetchRestaurantRequest;
 use http\Client\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class RestaurantController extends Controller
 {
-    public function fetch(Request $request, FetchRestaurantAction $action): JsonResponse
+    public function fetch(FetchRestaurantRequest $request, FetchRestaurantAction $action): JsonResponse
     {
-        $req = $request->validate([
-            'search' => ['nullable']
-        ]);
-        $restaurants = $action->execute($req);
-        return response()->json($restaurants);
+        $cacheKey = 'search-cache-' . trim($request['search']);
+        $restaurantData = Cache::remember($cacheKey, now()->addHours(24), function () use ($request, $action) {
+            $restaurants = $action->execute($request->validated());
+            return $restaurants;
+        });
+        return response()->json($restaurantData);
     }
 }
